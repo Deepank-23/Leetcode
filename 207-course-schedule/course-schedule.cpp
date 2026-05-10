85673b2745d91c9f90110d23f6b8588f2cdcddaa
Deepank Agarwal <deepankagarwal23@gmail.com>
2026-05-10 16:51:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (47.21%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,48 +1,18 @@
 class Solution {
 
-    bool bfs(vector<vector<int>>& adj,
-             vector<int>& indegree,
-             vector<int>& vis,
-             vector<int>& res){
-
-        queue<int> q;
-
-        int n = adj.size();
-
-        // push indegree 0 nodes
-        for(int i=0; i<n; i++){
-
-            if(indegree[i] == 0){
-
-                q.push(i);
-
-                vis[i] = 1;
-            }
+    bool dfs(vector<vector<int>>& adj,vector<int>& path,vector<int>& vis,int node){
+        vis[node]=1;
+        path[node]=1;
+        for(int i : adj[node]){
+            
+            if (!vis[i]) {
+                if (dfs(adj, path, vis, i)) return true; // propagate cycle found
+            } else if (path[i]) return true; // back edge = cycle
+            
         }
-
-        while(!q.empty()){
-
-            int front = q.front();
-
-            q.pop();
-
-            res.push_back(front);
-
-            for(int j : adj[front]){
-
-                indegree[j]--;
-
-                if(indegree[j] == 0 &&
-                   !vis[j]){
-
-                    q.push(j);
-
-                    vis[j] = 1;
-                }
-            }
-        }
-
-        return res.size() == n;
+        path[node]=0;
+        return false;
+        
     }
 
 public:
@@ -61,21 +31,15 @@ public:
             adj[v].push_back(u);
         }
 
-        vector<int> indegree(numCourses,0);
 
-        // calculate indegree
-        for(int i=0; i<numCourses; i++){
-
-            for(int j : adj[i]){
-
-                indegree[j]++;
-            }
-        }
 
         vector<int> vis(numCourses,0);
+        vector<int> path(numCourses,0);
+        for(int i =0 ; i<numCourses;i++){
+            if(!vis[i] && dfs(adj, path, vis, i)) return false;
+           
+        }
 
-        vector<int> res;
-
-        return bfs(adj,indegree,vis,res);
+        return true;
     }
 };
