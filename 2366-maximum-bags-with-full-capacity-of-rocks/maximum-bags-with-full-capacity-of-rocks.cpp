9607ec0cb48c5098fe90d1503251ce260b982c26
Deepank Agarwal <deepankagarwal23@gmail.com>
2026-07-23 18:55:06 +0530

    Time: 24 ms (27.45%) | Memory: 93.3 MB (33.24%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
-        vector<pair<int,int>>items;
-        for(int i =0; i<capacity.size();i++){
-            items.push_back({capacity[i],rocks[i]});
+
+        vector<int>need;
+
+        for(int i=0;i<capacity.size();i++){
+            need.push_back(capacity[i]-rocks[i]);
         }
 
-        sort(items.begin(),items.end(),[](auto &a,auto &b){
-            return (a.first-a.second)<(b.first-b.second);
-        });
-        int maxbag=0;
-        int left = additionalRocks;
-        for (int i=0;i<capacity.size();i++){
-            if(items[i].first-items[i].second!=0 &&items[i].first-items[i].second<=left){
-                left = left - (items[i].first-items[i].second);
-                maxbag++;
+        sort(need.begin(), need.end());
+
+        int ans=0;
+
+        for(int x: need){
+            if(x<=additionalRocks){
+                additionalRocks-=x;
+                ans++;
             }
-            else if (items[i].first-items[i].second==0){
-                maxbag++;
+            else{
+                break;
             }
         }
-        return maxbag;
 
+        return ans;
     }
 };
