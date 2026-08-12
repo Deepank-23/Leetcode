d2992c68f258355fee3b20063f30e5b8c370e5b9
Deepank Agarwal <deepankagarwal23@gmail.com>
2026-08-12 14:22:27 +0530

    Time: 45 ms (60.57%) | Memory: 13.2 MB (57.06%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -4,18 +4,23 @@ public:
         int srt = (int) sqrt(n);
         return (srt*srt==n);
     }
-    int minn(vector<int> &dp,int n){
-        if(perfect(n))return 1;
-        if(dp[n]!=0)return dp[n];
-        int minm = 34567;
-        for(int i=1;i*i<=n;i++){
-            int count = minn(dp,i*i)+minn(dp,n-(i*i));
-            minm= min(minm,count);
-        }
-        return dp[n]=minm;
-    }
+    
     int numSquares(int n) {
-        vector<int>dp(n+1,0);
-        return minn(dp,n);
+        vector<int>dp(n+1,-1);
+        
+        for(int i= 1;i<=n;i++){
+            if(perfect(i))dp[i]=1;
+            else{
+                int minm = INT_MAX;
+                for(int j=1;j*j<=i;j++){
+                    int count = dp[j*j]+dp[i-(j*j)];
+                    minm= min(minm,count);
+                }
+                dp[i]=minm;
+            }  
+
+        }
+        return dp[n];
+        
     }
 };
