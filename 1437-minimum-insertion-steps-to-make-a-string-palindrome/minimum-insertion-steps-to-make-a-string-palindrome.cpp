8879c7eff61f80ef83d0abb21a25168fee687354
Deepank Agarwal <deepankagarwal23@gmail.com>
2026-08-18 18:06:31 +0530

    Time: 31 ms (64.10%) | Memory: 31.7 MB (16.34%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int charact(string &s,int i , int j,vector<vector<int>>&dp){
-        if(i==j)return 0;
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==s[j])return dp[i][j]=charact(s,i+1,j-1,dp);
-        else return dp[i][j]= 1+ min(charact(s,i+1,j,dp),charact(s,i,j-1,dp));
-    }
+    int lcs(string& s ,int i ,int j,vector<vector<int>>& dp){
+            if(i>j)return 0;
+            if(i==j)return 1;
+            if(dp[i][j]!=-1)return dp[i][j];
+            if(s[i]==s[j]) return dp[i][j]=2+lcs(s,i+1,j-1,dp);
+            else return dp[i][j]=max(lcs(s,i+1,j,dp),lcs(s,i,j-1,dp));
+        }
+
+
     int minInsertions(string s) {
         int n = s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return charact(s,0,n-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        return s.size()-lcs(s,0,n-1,dp);
     }
 };
